3af09f20ccf76a92751a179fd8d232cc8393a99e
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2024-10-05 11:49:03 -0300

    :tada: init: Projeto Criado

---
+++ src/main/java/com/fernandocanabarro/desafio_goomer/DesafioGoomerApplication.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.desafio_goomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGoomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGoomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandocanabarro/desafio_goomer/DesafioGoomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.desafio_goomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioGoomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
